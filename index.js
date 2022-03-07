fa7a858ea4fcee563d0f8bcd9c7c810ea380171c
Mike Li <lim48687@gmail.com>
2022-03-07 16:28:59 +0100

    added index.html index.css index.js
    A Simple Unit Converter web applicaiton

---
+++ index.js
@@ -0,0 +1,47 @@
+// variable declaration
+// UserInput
+let inputEl = document.getElementById("user-input")
+let inputString = inputEl.value
+console.log(inputString)
+
+// Converting Units
+let lengthEl = document.getElementById("length-el");
+let volumeEl = document.getElementById("volume-el");
+let massEl = document.getElementById("mass-el");
+
+// Conversion funtions
+// Length function convert meter/feet
+function lenghtConversion() {
+   let meter = inputString / 3.28084;
+   let feet = inputString * 3.28084;
+   lengthEl.textContent = inputString + " meters = " + feet.toFixed(3) + " feet | " + inputString + " feet = " + meter.toFixed(3) + " meters";
+}
+
+// Volume function convert Liters/Gallons
+function volumeConversion() {
+    let liters = inputString * 3.78541;
+    let gallons = inputString / 3.78541;
+    volumeEl.textContent = inputString + " liters = " + gallons.toFixed(3) + " gallons | " + inputString + " gallons = " + liters.toFixed(3) + " liters";
+}
+
+// Mass function convert Killogrrams/Pounds
+function massConversion() {
+    let pounds = inputString *  2.20462;
+    let kilos = inputString /  2.20462;
+    massEl.textContent = inputString + " kilos = " + pounds.toFixed(3) + " pounds | " + inputString + " pounds = " + kilos.toFixed(3) + " kilos";
+    
+}
+
+// Output Conversion
+function conversion() {
+    inputString = inputEl.value
+    lenghtConversion()
+    volumeConversion()
+    massConversion()
+}
+
+
+
+function hello() {
+    lengthEl.textContent = "Hello World"
+}
